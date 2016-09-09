9062eec4b6a36abe971c29a0eff6451df4bcae23
Madeline Endres <endremad@umich.edu>
2016-09-09 13:48:16 -0400

    added getAllCityEateryNames

--- tripAdvisorScraper.py
+++ tripAdvisorScraper.py
@@ -7,7 +7,7 @@ import string
 #URL to the first page of the restaurent names we want to scrape
 BASE_URL = "http://www.tripadvisor.com/Restaurants-g29556-Ann_Arbor_Michigan.html"
 TOWN_NAME = "Ann_Arbor_Michigan"
-
+NUM_PER_PAGE = 30 
 
 def getNumRestaurents():
     """
@@ -27,7 +27,7 @@ def generatePageUrl(pageNum):
     Returns the URL specific to the page number specified
     Pages are 0 indexd
     """
-    pageStarterNum = (pageNum * 30) + 1
+    pageStarterNum = (pageNum * NUM_PER_PAGE) + 1
     splitIndex = string.find(BASE_URL, TOWN_NAME)
     newURL = BASE_URL[0:splitIndex] + "oa" + str(pageStarterNum) + "-" + BASE_URL[splitIndex:len(BASE_URL)]
     return newURL
@@ -36,7 +36,7 @@ def generatePageUrl(pageNum):
 def getEateryNamesAndURLs(section_url):
     """
     Gets the data on one page of the url (page specified by section url)
-    Returns a list of all 30 restaurent names on the page specified
+    Returns a list of all NUM_PER_PAGE restaurent names on the page specified
     Each name is paired with the url stub of the restaurent
     """
     html = urlopen(section_url).read()
@@ -45,17 +45,25 @@ def getEateryNamesAndURLs(section_url):
     search_results = soup.find_all(class_="property_title")
     #Formats what I need
     return [(i.string.replace('\n', ''), i.get('href')) for i in search_results]
-        
+
+
+def getAllCityEateryNames():
+    """
+    returns a list of all of the eartery names in the city with their
+    corresponding url stubs
+    """
+    numRestaurents = getNumRestaurents()
+    restaurents = []
+    for pageNum in range(0, numRestaurents / NUM_PER_PAGE + 1):
+        section_url = generatePageUrl(pageNum)
+        restaurents.extend(getEateryNamesAndURLs(section_url))
+    return restaurents
+
 
 def main():
 
-    #Gets a list of all restaurents tripadvisor has in ann arbor
-    numRestaurents = getNumRestaurents()
-    restaurents = []
-    for pageNum in range(0, numRestaurents / 30 + 1):
-        section_url = generatePageUrl(pageNum)
-        restaurents.extend(getEateryNamesAndURLs(section_url))
-
+    #Gets a list of all restaurents tripadvisor has in the city specified up top
+    restaurents = getAllCityEateryNames()
     for i in restaurents:
         print i[0]
 
